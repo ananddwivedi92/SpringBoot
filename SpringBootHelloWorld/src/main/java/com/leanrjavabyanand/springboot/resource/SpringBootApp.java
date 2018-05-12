2954daf95eec08f42ac8eac871cb3ded3e5a9060
Anand <Anand@Anand-PC>
2018-05-12 16:32:28 +0530

    Second Commit

---
+++ SpringBootHelloWorld/src/main/java/com/leanrjavabyanand/springboot/resource/SpringBootApp.java
@@ -0,0 +1,13 @@
+package com.leanrjavabyanand.springboot.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Anand
+ */
+public class SpringBootApp {
+	public static void main(String[] args) {
+		System.out.println("OK");
+	}
+}
